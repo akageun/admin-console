142bf2d0c9ba3114548e8d9fadad92925423b2ee
akageun <akageun@gmail.com>
2019-10-02 01:07:07 +0900

    semantic ui 적용 중간커밋

--- admin/src/main/frontend/App.vue
+++ admin/src/main/frontend/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <!-- Sidebar -->
-        <div class="ui right demo vertical sidebar labeled icon menu" id="mobile-sidebar">
+    <sui-sidebar-pushable>
+        <sui-sidebar animation="overlay" class="inverted" :visible="visible">
+            <!-- links -->
             <a class="item">
                 <i class="sticky note icon"></i>
                 Bulletin
@@ -14,64 +14,108 @@
                 <i class="mail icon"></i>
                 Contact us
             </a>
-        </div>
+        </sui-sidebar>
+        <sui-sidebar-pusher :dimmed="visible" @click="visible = false">
+            <sui-menu fixed inverted>
+                <sui-container fluid>
+                    <a is="sui-menu-item" @click.stop="visible = true">Menu</a>
 
-        <div class="ui fixed inverted main menu mobile-menu">
-            <div class="ui container">
-                <div class="item">
-                    Menu
-                </div>
-                <div class="right menu">
-
-                    <a class="launch icon item" id="mobile-menu-trigger" v-on:click="toggle">
-                        <i class="content icon"></i>
+                    <a href="#" is="sui-menu-item" class="header">
+                        Admin Console
                     </a>
-                </div>
-            </div>
-        </div>
-        <div class="pusher">
-            <h1>
-                Hello Vue
-            </h1>
-        </div>
 
-    </div>
+                    <a is="sui-menu-item" href="#">Home</a>
+
+                    <sui-dropdown text="Dropdown" item class="simple">
+                        <sui-dropdown-menu>
+                            <a is="sui-dropdown-item" href="#">Link Item</a>
+                            <a is="sui-dropdown-item" href="#">Link Item</a>
+
+                            <sui-dropdown-divider/>
+                            <sui-dropdown-header>Header Item</sui-dropdown-header>
+
+                            <sui-dropdown-item>
+                                <sui-icon name="dropdown"/>
+                                Submenu
+
+                                <sui-dropdown-menu class="simple">
+                                    <a is="sui-dropdown-item" href="#">Link Item</a>
+                                    <a is="sui-dropdown-item" href="#">Link Item</a>
+                                </sui-dropdown-menu>
+                            </sui-dropdown-item>
+
+                            <a is="sui-dropdown-item" href="#">Link Item</a>
+                        </sui-dropdown-menu>
+                    </sui-dropdown>
+                </sui-container>
+            </sui-menu>
+
+            <router-view/>
+
+            <sui-segment inverted vertical class="footer">
+                <sui-container fluid text-align="center">
+                    <sui-grid class="inverted" stackable divided>
+                        <sui-grid-row>
+                            <sui-grid-column :width="3" v-for="n in 3" :key="n">
+                                <sui-list class="inverted" link>
+                                    <h4 is="sui-header" :content="`Group ${n}`" class="inverted"/>
+                                    <a is="sui-list-item" href="#">Link One</a>
+                                    <a is="sui-list-item" href="#">Link Two</a>
+                                    <a is="sui-list-item" href="#">Link Three</a>
+                                    <a is="sui-list-item" href="#">Link Four</a>
+                                </sui-list>
+                            </sui-grid-column>
+                            <sui-grid-column :width="3">
+                                <h4 is="sui-header" content="Footer Header" class="inverted"/>
+                                <p>
+                                    Extra space for a call to action inside the footer that could help re-engage users.
+                                </p>
+                            </sui-grid-column>
+                        </sui-grid-row>
+                    </sui-grid>
+
+                    <sui-divider inverted section/>
+
+                    <sui-list
+                        horizontal
+                        divided
+                        link
+                        class="inverted">
+
+                        <a is="sui-list-item" href="#">Site Map</a>
+                        <a is="sui-list-item" href="#">Contact Us</a>
+                        <a is="sui-list-item" href="#">Terms and Conditions</a>
+                        <a is="sui-list-item" href="#">Privacy Policy</a>
+                    </sui-list>
+                </sui-container>
+            </sui-segment>
+        </sui-sidebar-pusher>
+    </sui-sidebar-pushable>
 </template>
 
 <script>
     export default {
         name: 'App',
-        data: function () {
-            return {
-                isToggled: false,
-                sideBar: null
-            }
-        },
-        methods: {
-            toggle: function () {
-                if (this.isToggled) {
-                    this.sideBar.sidebar('hide')
-                } else {
-                    this.sideBar.sidebar('show')
-                }
-            }
-        },
-        mounted: function () {
-            this.sideBar = document.getElementById('mobile-sidebar');
-            let self = this
-            this.sideBar.sidebar({
-                transition: 'push',
-                onHidden: function () {
-                    self.isToggled = false
-                },
-                onShow: function () {
-                    self.isToggled = true
-                }
-            })
-        }
+        data: () => ({
+            visible: false
+        }),
 
     }
 </script>
 
 <style>
+    html, body {
+        min-height: 100%;
+        overflow: hidden
+    }
+
+    .main {
+        margin-top: 70px;
+        height: inherit;
+    }
+
+    .footer {
+        margin: 5em 0em 0em;
+        padding: 5em 0em;
+    }
 </style>
--- admin/src/main/frontend/views/home.vue
+++ admin/src/main/frontend/views/home.vue
@@ -1,9 +1,38 @@
 <template>
-    <div class="hello">
+    <sui-container class="main" text>
+        <sui-header size="huge">Semantic UI Vue Fixed Template</sui-header>
+        <p>This is a basic fixed menu template using fixed size containers.</p>
+        <p>
+            A text container is used for the main container, which is useful for single column layouts.
+        </p>
+
         <h1>{{ msg }}</h1>
         <h2>Essential Links</h2>
-        <img src="/static/bundle/assets/logo.png">
-    </div>
+
+        <h2 is="sui-header">Dogs Roles with Humans</h2>
+        <p>
+            Domestic dogs inherited complex behaviors, such as bite inhibition, from
+            their wolf ancestors, which would have been pack hunters with complex body
+            language. These sophisticated forms of social cognition and communication
+            may account for their trainability, playfulness, and ability to fit into
+            human households and social situations, and these attributes have given
+            dogs a relationship with humans that has enabled them to become one of the
+            most successful species on the planet today.
+        </p>
+
+        <p>
+            The dogs' value to early human hunter-gatherers led to them quickly
+            becoming ubiquitous across world cultures. Dogs perform many roles for
+            people, such as hunting, herding, pulling loads, protection, assisting
+            police and military, companionship, and, more recently, aiding handicapped
+            individuals. This impact on human society has given them the nickname
+            "man's best friend" in the Western world. In some cultures, however,
+            dogs are also a source of meat.
+        </p>
+
+<!--        <sui-image src="/static/bundle/assets/logo.png" size="mini" centered/>-->
+        <sui-image src="/static/bundle/assets/logo.png" centered/>
+    </sui-container>
 </template>
 
 <script>
@@ -23,21 +52,6 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
 </style>
