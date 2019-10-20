0e7ad68b910d3f9f6e53b3bfd9ec177007fadf63
akageun <akageun@gmail.com>
2019-10-20 21:30:24 +0900

    회원가입 페이지

--- admin/src/main/java/kr/geun/oss/admin/routes/user/SignupApi.java
+++ admin/src/main/java/kr/geun/oss/admin/routes/user/SignupApi.java
@@ -1,8 +1,12 @@
 package kr.geun.oss.admin.routes.user;
 
+import kr.geun.oss.base.app.user.model.SignupModel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * SignupApi
@@ -16,8 +20,11 @@ public class SignupApi {
 
     @PostMapping("/api/signup/v1")
     public String signup(
-
+        @Valid SignupModel param,
+        BindingResult result
     ) {
+        log.info("param :{}", param);
+        log.info("result : {}", result);
 
         return "OK";
     }
--- base/src/main/java/kr/geun/oss/base/app/user/model/SignupModel.java
+++ base/src/main/java/kr/geun/oss/base/app/user/model/SignupModel.java
@@ -2,8 +2,13 @@ package kr.geun.oss.base.app.user.model;
 
 import kr.geun.oss.base.common.model.BaseModel;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 /**
  * SignupModel
@@ -12,10 +17,24 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2019-10-16
  */
 @Slf4j
+@Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignupModel extends BaseModel {
 
+    private String userId;
+    private String passWd;
+    private String confirmPassWd;
     private String email;
-    private String passwd;
 
+    @Builder
+    public SignupModel(
+        @NotBlank String userId,
+        @NotBlank String passWd,
+        @NotBlank String confirmPassWd,
+        @Email String email) {
+        this.userId = userId;
+        this.passWd = passWd;
+        this.confirmPassWd = confirmPassWd;
+        this.email = email;
+    }
 }
